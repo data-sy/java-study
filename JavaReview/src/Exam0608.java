caf27bae17123b98db09284a744be145b5779b72
Lee Soyeon <data.sy.2@gmail.com>
2022-09-21 21:16:59 +0900

    배열 실습 - 배열 안의 수만큼 별찍기

--- JavaReview/src/Exam0608.java
+++ JavaReview/src/Exam0608.java
@@ -20,6 +20,17 @@ public class Exam0608 {
 		System.out.println(Arrays.toString(arr));
 		System.out.println(String.format("가장 큰 값은 %d입니다.", max));
 		
+		
+		// 실습 3 - 배열 안의 수만큼 별찍기
+		int[] starCount = {3, 4, 4, 2, 1};
+		
+		for (int i=0; i<starCount.length; i++) {
+			System.out.print(starCount[i]+" : ");
+			for (int j=0; j<starCount[i]; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}		
 
 	}
 

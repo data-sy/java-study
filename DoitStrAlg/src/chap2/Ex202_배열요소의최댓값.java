c20a1c0a46b7359acfba595c1ef3faaade082f26
Lee Soyeon <data.sy.2@gmail.com>
2022-09-30 22:39:45 +0900

    57p까지

---
+++ DoitStrAlg/src/chap2/Ex202_배열요소의최댓값.java
@@ -0,0 +1,22 @@
+package chap2;
+
+public class Ex202_배열요소의최댓값 {
+
+	public static void main(String[] args) {
+		
+		int[] a = new int[5];
+		a[1]=37;
+		a[2]=51;
+		a[4]=a[1]*2;
+		
+		int max = a[0];
+		for (int i=1; i<a.length; i++) {
+			if (a[i]>max) max =a[i];
+		}
+		// 길이가 n인 배열이면, if조건 n-1회
+
+		// TODO Auto-generated method stub
+
+	}
+
+}

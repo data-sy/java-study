64264d14dac190f4c9aad26fa739fca5d1a2ae0e
Lee Soyeon <data.sy.2@gmail.com>
2023-11-24 09:56:35 +0900

    jpa, jdbc도 같이 잘 동작하는지 확인 - 성공

--- neo4j/src/main/java/com/mmt/neo4j/DeveloperController.java
+++ neo4j/src/main/java/com/mmt/neo4j/DeveloperController.java
@@ -2,7 +2,10 @@ package com.mmt.neo4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DeveloperController {
@@ -10,9 +13,20 @@ public class DeveloperController {
     @Autowired
     private DeveloperService developerService;
 
+
     @GetMapping("/")
-    public  Developer findAmud(){
+    public Developer findAmud(){
         return  developerService.findAmud();
+    }
+
+    @GetMapping("/sections")
+    public List<Sections> findSections(){
+        return developerService.findSections();
+    }
+
+    @GetMapping("/jdbc")
+    public List<String> findJdbcSections(){
+        return developerService.findByJdbc();
     }
 
 }
--- neo4j/src/main/java/com/mmt/neo4j/DeveloperService.java
+++ neo4j/src/main/java/com/mmt/neo4j/DeveloperService.java
@@ -3,12 +3,29 @@ package com.mmt.neo4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DeveloperService {
     @Autowired
     private DeveloperRepository developerRepository;
 
+    @Autowired
+    private SectionRepository sectionRepository;
+
+    @Autowired
+    private  SectionJdbcRepository sectionJdbcRepository;
+
     public Developer findAmud(){
         return developerRepository.findOneByName("amud").block();
     }
+
+    public List<Sections> findSections(){
+        return sectionRepository.findAll();
+    }
+
+    public List<String> findByJdbc(){
+        return sectionJdbcRepository.findAllSection();
+    }
+
 }

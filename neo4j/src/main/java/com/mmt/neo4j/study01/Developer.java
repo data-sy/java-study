c6447e3db24a99a0a611806c8c1e36a6840cf444
Lee Soyeon <data.sy.2@gmail.com>
2023-12-02 17:18:03 +0900

    study03와 중복방지를 위해 코드 주석 처리

--- neo4j/src/main/java/com/mmt/neo4j/study01/Developer.java
+++ neo4j/src/main/java/com/mmt/neo4j/study01/Developer.java
@@ -1,30 +1,30 @@
-package com.mmt.neo4j.study01;
-
-import com.mmt.neo4j.study02.PersonEntity;
-import lombok.Getter;
-import org.springframework.data.neo4j.core.schema.Id;
-import org.springframework.data.neo4j.core.schema.Node;
-import org.springframework.data.neo4j.core.schema.Relationship;
-
-import java.util.HashSet;
-import java.util.Set;
-
-import static org.springframework.data.neo4j.core.schema.Relationship.Direction.INCOMING;
-
-@Getter
-@Node("Developer")
-public class Developer {
-
-    @Id
-    private String name;
-    private String country;
-    private int age;
-    @Relationship(type = "IS_FRIEND", direction = INCOMING)
-    private Set<Developer> friends = new HashSet<>();
-
-    public Developer(String name, String country, int age){
-        this.name = name;
-        this.country = country;
-        this.age = age;
-    }
-}
+//package com.mmt.neo4j.study01;
+//
+//import com.mmt.neo4j.study02.PersonEntity;
+//import lombok.Getter;
+//import org.springframework.data.neo4j.core.schema.Id;
+//import org.springframework.data.neo4j.core.schema.Node;
+//import org.springframework.data.neo4j.core.schema.Relationship;
+//
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//import static org.springframework.data.neo4j.core.schema.Relationship.Direction.INCOMING;
+//
+//@Getter
+//@Node("Developer")
+//public class Developer {
+//
+//    @Id
+//    private String name;
+//    private String country;
+//    private int age;
+//    @Relationship(type = "IS_FRIEND", direction = INCOMING)
+//    private Set<Developer> friends = new HashSet<>();
+//
+//    public Developer(String name, String country, int age){
+//        this.name = name;
+//        this.country = country;
+//        this.age = age;
+//    }
+//}
--- neo4j/src/main/java/com/mmt/neo4j/study01/Sections.java
+++ neo4j/src/main/java/com/mmt/neo4j/study01/Sections.java
@@ -1,20 +1,20 @@
-package com.mmt.neo4j.study01;
-
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.*;
-
-@Entity
-@Getter
-@Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class Sections {
-
-    @Id
-    private int sectionId;
-
-    private String sectionName;
-}
+//package com.mmt.neo4j.study01;
+//
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import lombok.*;
+//
+//@Entity
+//@Getter
+//@Setter
+//@Builder
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class Sections {
+//
+//    @Id
+//    private int sectionId;
+//
+//    private String sectionName;
+//}

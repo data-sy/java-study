67ae1092696224330b097a15bee9f4cc0a640acf
Lee Soyeon <data.sy.2@gmail.com>
2023-12-02 22:37:15 +0900

    학습 끝낸 실습 데이터는 주석 처리

--- neo4j/src/main/java/com/mmt/neo4j/study02/MovieController.java
+++ neo4j/src/main/java/com/mmt/neo4j/study02/MovieController.java
@@ -1,36 +1,36 @@
-package com.mmt.neo4j.study02;
-
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-@RestController
-@RequestMapping("/movies")
-public class MovieController {
-    private final MovieService movieService;
-    public MovieController(MovieService movieService) {
-        this.movieService = movieService;
-    }
-
-    @PutMapping
-    public Mono<MovieEntity> createOrUpdateMovie(@RequestBody MovieEntity newMovie) {
-        return movieService.createOrUpdateMovie(newMovie);
-    }
-
-    @GetMapping(value = { "", "/" }, produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<MovieEntity> getMovies() {
-        return movieService.getMovies();
-    }
-
-    @GetMapping("/by-title")
-    public Mono<MovieEntity> byTitle(@RequestParam String title) {
-        return movieService.byTitle(title);
-    }
-
-    @DeleteMapping("/{id}")
-    public Mono<Void> delete(@PathVariable String id) {
-        return movieService.delete(id);
-    }
-
-}
+//package com.mmt.neo4j.study02;
+//
+//import org.springframework.http.MediaType;
+//import org.springframework.web.bind.annotation.*;
+//import reactor.core.publisher.Flux;
+//import reactor.core.publisher.Mono;
+//
+//@RestController
+//@RequestMapping("/movies")
+//public class MovieController {
+//    private final MovieService movieService;
+//    public MovieController(MovieService movieService) {
+//        this.movieService = movieService;
+//    }
+//
+//    @PutMapping
+//    public Mono<MovieEntity> createOrUpdateMovie(@RequestBody MovieEntity newMovie) {
+//        return movieService.createOrUpdateMovie(newMovie);
+//    }
+//
+//    @GetMapping(value = { "", "/" }, produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+//    public Flux<MovieEntity> getMovies() {
+//        return movieService.getMovies();
+//    }
+//
+//    @GetMapping("/by-title")
+//    public Mono<MovieEntity> byTitle(@RequestParam String title) {
+//        return movieService.byTitle(title);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public Mono<Void> delete(@PathVariable String id) {
+//        return movieService.delete(id);
+//    }
+//
+//}
--- neo4j/src/main/java/com/mmt/neo4j/study02/PersonEntity.java
+++ neo4j/src/main/java/com/mmt/neo4j/study02/PersonEntity.java
@@ -1,18 +1,18 @@
-package com.mmt.neo4j.study02;
-
-import lombok.Getter;
-import org.springframework.data.neo4j.core.schema.Id;
-import org.springframework.data.neo4j.core.schema.Node;
-
-@Getter
-@Node("Person")
-public class PersonEntity {
-    @Id
-    private final String name;
-    private final Integer born;
-    public PersonEntity(Integer born, String name) {
-        this.born = born;
-        this.name = name;
-    }
-
-}
+//package com.mmt.neo4j.study02;
+//
+//import lombok.Getter;
+//import org.springframework.data.neo4j.core.schema.Id;
+//import org.springframework.data.neo4j.core.schema.Node;
+//
+//@Getter
+//@Node("Person")
+//public class PersonEntity {
+//    @Id
+//    private final String name;
+//    private final Integer born;
+//    public PersonEntity(Integer born, String name) {
+//        this.born = born;
+//        this.name = name;
+//    }
+//
+//}

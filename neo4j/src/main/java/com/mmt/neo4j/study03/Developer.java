f15af50149ec8e683962bc60a2f291bd63aee0e2
Lee Soyeon <data.sy.2@gmail.com>
2023-12-09 20:51:27 +0900

    일부 속성만 리스트로 추출하는 쿼리 테스트
    - collect() 함수로 시도했으나 java에서 어떤 타입으로 받아야 할지 못 찾았음
    - Neo4j 공식 문서에 있는 쿼리문 사용하여 [data in dataList] 사용

--- neo4j/src/main/java/com/mmt/neo4j/study03/Developer.java
+++ neo4j/src/main/java/com/mmt/neo4j/study03/Developer.java
@@ -1,32 +1,32 @@
-//package com.mmt.neo4j.study03;
-//
-//import lombok.Data;
-//import lombok.Getter;
-//import lombok.Setter;
-//import org.springframework.data.neo4j.core.schema.Id;
-//import org.springframework.data.neo4j.core.schema.Node;
-//import org.springframework.data.neo4j.core.schema.Relationship;
-//
-//import java.util.HashSet;
-//import java.util.Set;
-//
-//import static org.springframework.data.neo4j.core.schema.Relationship.Direction.INCOMING;
-//
-//@Data
-//@Node("Developer")
-//public class Developer {
-//
-//    @Id
-//    private String name;
-//    private String country;
-//    private int age;
-//    @Relationship(type = "IS_FRIEND", direction = INCOMING)
-//    private Set<Developer> friends = new HashSet<>();
-//
-//    public Developer(String name, String country, int age){
-//        this.name = name;
-//        this.country = country;
-//        this.age = age;
-//    }
-//
-//}
+package com.mmt.neo4j.study03;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.springframework.data.neo4j.core.schema.Relationship.Direction.INCOMING;
+
+@Data
+@Node("Developer")
+public class Developer {
+
+    @Id
+    private String name;
+    private String country;
+    private int age;
+    @Relationship(type = "IS_FRIEND", direction = INCOMING)
+    private Set<Developer> friends = new HashSet<>();
+
+    public Developer(String name, String country, int age){
+        this.name = name;
+        this.country = country;
+        this.age = age;
+    }
+
+}
--- neo4j/src/main/java/com/mmt/neo4j/study03/DeveloperService.java
+++ neo4j/src/main/java/com/mmt/neo4j/study03/DeveloperService.java
@@ -1,34 +1,46 @@
-//package com.mmt.neo4j.study03;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//import reactor.core.publisher.Flux;
-//
-//import java.util.List;
-//
-//@Service
-//public class DeveloperService {
-//    @Autowired
-//    private DeveloperRepository developerRepository;
-//
-//    public Developer findAmud(){
-//        return developerRepository.findOneByName("amud").block();
+package com.mmt.neo4j.study03;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.neo4j.core.ReactiveNeo4jClient;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+@Service
+public class DeveloperService {
+    @Autowired
+    private DeveloperRepository developerRepository;
+
+    private final ReactiveNeo4jClient neo4jClient;
+
+    public DeveloperService(ReactiveNeo4jClient neo4jClient) {
+        this.neo4jClient = neo4jClient;
+    }
+
+    public Developer findAmud(){
+        return developerRepository.findOneByName("amud").block();
+    }
+
+    public Developer findRobin(){
+        return developerRepository.findOneByName("robin").block();
+    }
+    public Developer findFriend(String name){
+        return developerRepository.findOneByName(name).block();
+    }
+
+    public Flux<Developer> findNodes(String name){
+        return developerRepository.findNodes(name);
+    }
+
+//    public Flux<PathResult> findObject(String name){
+//        return developerRepository.findNodesAndRelationships(name);
 //    }
-//
-//    public Developer findRobin(){
-//        return developerRepository.findOneByName("robin").block();
-//    }
-//    public Developer findFriend(String name){
-//        return developerRepository.findOneByName(name).block();
-//    }
-//
-//    public Flux<Developer> findNodes(String name){
-//        return developerRepository.findNodes(name);
-//    }
-//
-////    public Flux<PathResult> findObject(String name){
-////        return developerRepository.findNodesAndRelationships(name);
-////    }
-//
-//
-//}
+
+    public Flux<String> findNames(){
+        return developerRepository.findNames();
+    }
+
+
+    }
